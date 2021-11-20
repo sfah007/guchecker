3ae323881d8c914fce2cde1d289cab2cb4894866
sfah007 <69042482+sfah007@users.noreply.github.com>
2021-11-20 16:38:25 +0300

    Update BrainTree2.vue

--- src/components/BrainTree2.vue
+++ src/components/BrainTree2.vue
@@ -314,7 +314,7 @@ export default {
 
       var config = {
         method: "post",
-        url: "https://asterian.dev/b32.php",
+        url: "http://localhost/b32.php",
         headers: {
           "Content-Type": "application/json",
         },
