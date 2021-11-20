5f7e62eab39aa70acc7d7234e41c42846be442c3
sfah007 <69042482+sfah007@users.noreply.github.com>
2021-11-20 16:40:59 +0300

    Update VIP.vue

--- src/components/VIP.vue
+++ src/components/VIP.vue
@@ -37,7 +37,7 @@ export default {
   },
   created() {
     axios
-      .post("https://asterian.dev/get.php", {
+      .post("http://localhost/get.php", {
         pass: "GUGUGUGUGU",
       })
       .then(({ data }) => {
